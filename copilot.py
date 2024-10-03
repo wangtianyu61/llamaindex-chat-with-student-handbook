d8e9bda25d6abb146073035953504f42cb406b25
Tianyi Peng <tianyipeng95@gmail.com>
2024-10-03 00:24:37 -0400

    first commit

---
+++ copilot.py
@@ -0,0 +1,93 @@
+
+from openai import OpenAI
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from weather_api import get_n_day_weather_forecast
+from tenacity import retry, wait_random_exponential, stop_after_attempt
+import json
+import IPython;
+@retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(5))
+def chat_completion_request(client, messages, model="gpt-4o",
+                            **kwargs):
+    try:
+        response = client.chat.completions.create(
+            model=model,
+            messages=messages,
+            **kwargs
+        )
+        return response
+    except Exception as e:
+        print("Unable to generate ChatCompletion response")
+        print(f"Exception: {e}")
+        return e
+
+class Copilot:
+    def __init__(self):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        embedding_model = HuggingFaceEmbedding(
+            model_name="BAAI/bge-small-en"
+        )
+        self.index = VectorStoreIndex.from_documents(docs, embed_model = embedding_model,
+                                                     show_progress=True)
+        self.retriever = self.index.as_retriever(
+                        similarity_top_k=3
+                        )
+
+        self.llm_client = OpenAI()
+        
+        self.system_prompt = """
+            You are an expert on Columbia University and your job is to answer questions 
+            about the university.
+        """
+
+    def ask(self, question, messages):
+        ### use the retriever to get the answer
+        nodes = self.retriever.retrieve(question)
+        ### make answer a string with "1. <>, 2. <>, 3. <>"
+        retrieved_info = "\n".join([f"{i+1}. {node.text}" for i, node in enumerate(nodes)])
+        
+
+        processed_query_prompt = """
+            The user is asking a question: {question}
+
+            The retrived information is: {retrieved_info}
+
+            Please answer the question based on the retrieved information. If the question is not related to Columbia University, please tell the user and ask for a question related to Columbia University.
+
+            Please highlight the information and format the answer in markdown.
+        """
+        
+        processed_query = processed_query_prompt.format(question=question, 
+                                                        retrieved_info=retrieved_info)
+        
+        messages = [{"role": "system", "content": self.system_prompt}] + messages + [{"role": "user", "content": processed_query}]
+        response = chat_completion_request(self.llm_client, 
+                                           messages = messages, 
+                                           stream=True)
+        
+        return retrieved_info, response
+
+if __name__ == "__main__":
+    copilot = Copilot()
+    messages = []
+    while True:
+        question = input("Please ask a question: ")
+        retrived_info, answer = copilot.ask(question, messages=messages)
+        ### answer can be a generator or a string
+
+        #print(retrived_info)
+        if isinstance(answer, str):
+            print(answer)
+        else:
+            answer_str = ""
+            for chunk in answer:
+                content = chunk.choices[0].delta.content
+                if content:
+                    answer_str += content
+                    print(content, end="", flush=True)
+            print()
+            answer = answer_str
+
+        messages.append({"role": "user", "content": question})
+        messages.append({"role": "assistant", "content": answer})
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,40 @@
 import streamlit as st
 import openai
-from llama_index.llms.openai import OpenAI
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from copilot import Copilot
 
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-openai.api_key = st.secrets.openai_key
-st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
-st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+st.set_page_config(page_title="Chat with Columbia Copilot", layout="centered", initial_sidebar_state="auto", menu_items=None)
+### set openai key, first check if it is in environment variable, if not, check if it is in streamlit secrets, if not, raise error
+import os
+
+st.write(
+    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+)
+
+openai_key = os.getenv("OPENAI_API_KEY")
+if not openai_key:
+    openai_key = st.secrets.openai_key
+
+if not openai_key: ### get openai key from user input
+    openai_api_key = st.text_input("Please enter your OpenAI API Key", type="password")
+    if not openai_key:
+        raise st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+
+openai.api_key = openai_key
+st.title("Chat with Columbia Copilot 💬🦙")
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
-        {
-            "role": "assistant",
-            "content": "Ask me a question about Streamlit's open-source Python library!",
-        }
+        {"role": "assistant", "content": "I am Columbia Copilot, your personal assistant. You can ask me about Columbia University."}
     ]
 
-@st.cache_resource(show_spinner=False)
-def load_data():
-    reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-    docs = reader.load_data()
-    Settings.llm = OpenAI(
-        model="gpt-3.5-turbo",
-        temperature=0.2,
-        system_prompt="""You are an expert on 
-        the Streamlit Python library and your 
-        job is to answer technical questions. 
-        Assume that all questions are related 
-        to the Streamlit Python library. Keep 
-        your answers technical and based on 
-        facts – do not hallucinate features.""",
-    )
-    index = VectorStoreIndex.from_documents(docs)
-    return index
+@st.cache_resource
+def load_copilot():
+    return Copilot()
 
 
-index = load_data()
 
-if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
-    st.session_state.chat_engine = index.as_chat_engine(
-        chat_mode="condense_question", verbose=True, streaming=True
-    )
+if "chat_copilot" not in st.session_state.keys():  # Initialize the chat engine
+    st.session_state.chat_copilot = load_copilot()
 
 if prompt := st.chat_input(
     "Ask a question"
@@ -54,8 +48,26 @@ for message in st.session_state.messages:  # Write message history to UI
 # If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
-        response_stream = st.session_state.chat_engine.stream_chat(prompt)
-        st.write_stream(response_stream.response_gen)
-        message = {"role": "assistant", "content": response_stream.response}
-        # Add response to message history
-        st.session_state.messages.append(message)
+
+        retrived_info, answer = st.session_state.chat_copilot.ask(prompt, messages=st.session_state.messages[:-1])
+        ### answer can be a generator or a string
+
+        #print(retrived_info)
+        if isinstance(answer, str):
+            st.write(answer)
+        else:
+            ### write stream answer to UI
+            def generate():
+                for chunk in answer:
+                    content = chunk.choices[0].delta.content
+                    if content:
+                        yield content
+            answer = st.write_stream(generate())
+
+        st.session_state.messages.append({"role": "assistant", "content": answer})
+
+        # response_stream = st.session_state.chat_copilot.ask(prompt, messages=st.session_state.messages[:-1])
+        # st.write_stream(response_stream.response_gen)
+        # message = {"role": "assistant", "content": response_stream.response}
+        # # Add response to message history
+        # st.session_state.messages.append(message)
